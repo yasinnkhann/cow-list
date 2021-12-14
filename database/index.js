740511862012eec65807ad94907a7af7cf2e83a8
Yasin Khan <yasinkhan500@gmail.com>
2021-12-13 17:39:11 -0800

    implemented get and post

--- database/index.js
+++ database/index.js
@@ -2,9 +2,9 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'student',
-  password: 'student',
-  database: 'YOUR_DATABASE_NAME_HERE'
+  user: 'root',
+  password: 'password',
+  database: 'cowlist2'
 });
 
 connection.connect((err) => {
@@ -18,10 +18,28 @@ connection.connect((err) => {
 // Your Database Queries Here!!
 
 
-
-
-
 // Don't forget to export your functions!
 module.exports = {
+  // db: connection,
+  getAll: function(cb) {
+    connection.query('SELECT * FROM cows2;', (err, data) => {
+      if (err) {
+        cb(err, null);
+      } else {
+        cb(null, data);
+      }
+    })
+  },
+  create: function(cowDetails, cb) {
+    const name = cowDetails.name;
+    const description = cowDetails.description;
 
+    connection.query('INSERT INTO cows2 (name, description) VALUES (?, ?);', [name, description], (err, data) => {
+      if (err) {
+        cb(err, null);
+      } else {
+        cb(null, data);
+      }
+    })
+  }
 };
