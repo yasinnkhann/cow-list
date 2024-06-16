394c6c3a6c58aec3ed39d0c219084545b8d706c9
Yasin Khan <yasinkhan500@gmail.com>
2024-06-16 16:26:10 -0700

    stashe

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,18 +2,13 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    // BINDERS
-  };
+	constructor(props) {
+		super(props);
+		this.state = {};
+		// BINDERS
+	}
 
-  render() {
-
-    return (
-      <div>
-        Hey
-      </div>
-    );
-  }
+	render() {
+		return <div>Hey</div>;
+	}
 }
--- database/index.js
+++ database/index.js
@@ -1,68 +1,71 @@
 const mongoose = require('mongoose');
 
 const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 };
 
 mongoose.connect('mongodb://localhost/cowList', options);
 
 const cowSchema = mongoose.Schema({
-  name: String,
-  description: String
+	name: String,
+	description: String,
 });
 
 const Cow = mongoose.model('Cow', cowSchema);
 
 const getCows = async cb => {
-  try {
-    const cows = await Cow.find({});
-    cb(cows, null);
-  } catch (err) {
-    cb(err, null);
-  }
+	try {
+		const cows = await Cow.find({});
+		cb(cows, null);
+	} catch (err) {
+		cb(err, null);
+	}
 };
 
 const postCow = async (cowDetails, cb) => {
-  const { name, description } = cowDetails;
+	const { name, description } = cowDetails;
 
-  try {
-    const editedCow = {
-      name,
-      description
-    };
+	try {
+		const editedCow = {
+			name,
+			description,
+		};
 
-    const freshCow = new Cow(editedCow);
-    const savedCow = await freshCow.save();
-    cb(null, savedCow._doc);
-  } catch (err) {
-    cb(err, null);
-  }
+		const freshCow = new Cow(editedCow);
+		const savedCow = await freshCow.save();
+		cb(null, savedCow._doc);
+	} catch (err) {
+		cb(err, null);
+	}
 };
 
 const editCow = async (id, newCowInfo, cb) => {
-  const { name, description } = newCowInfo;
+	const { name, description } = newCowInfo;
 
-  try {
-    const updatedCow = await Cow.findOneAndUpdate({ _id: id }, { name, description })
-    cb(null, updatedCow._doc)
-  } catch (err) {
-    cb(err, null);
-  }
+	try {
+		const updatedCow = await Cow.findOneAndUpdate(
+			{ _id: id },
+			{ name, description }
+		);
+		cb(null, updatedCow._doc);
+	} catch (err) {
+		cb(err, null);
+	}
 };
 
 const deleteCow = async (id, cb) => {
-  try {
-    const deletedCow = await Cow.deleteOne({ _id: id });
-    cb(null, deletedCow);
-  } catch (err) {
-    cb(err, null);
-  }
+	try {
+		const deletedCow = await Cow.deleteOne({ _id: id });
+		cb(null, deletedCow);
+	} catch (err) {
+		cb(err, null);
+	}
 };
 
 module.exports = {
-  getCows,
-  postCow,
-  editCow,
-  deleteCow
+	getCows,
+	postCow,
+	editCow,
+	deleteCow,
 };
